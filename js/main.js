458a15fb27fc4afe7626fce761f0ee05c5443582
msrikar98 <srikarstriker007@gmail.com>
2017-03-09 03:50:45 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -169,3 +169,20 @@ function getRandom(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+$(function() {
+  $( "#button" ).click(function() {
+    $( "#button" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button" ).removeClass( "onclic" );
+      $( "#button" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
